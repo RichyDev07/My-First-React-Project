775bfddb68bd1f268a4bba0e0ee4ae602119aada
RichyDev07 <richmondgyening007@gmail.com>
2024-02-20 03:09:44 -0800

    creating first react project

---
+++ Hello-World/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Greet} from './component/Greet';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ Hello-World/src/component/Greet.js
@@ -0,0 +1,9 @@
+import react from 'react'
+
+// function Greet() {
+//     return <h1> Hello Rich </h1>
+
+     
+// }
+export const Greet = () => <h1> Hello Rich </h1>
+// export default Greet
